61db7988cf65c3f3f6cfebd481989f05a2605bf8
chance-404 <chance404404@gmail.com>
2025-08-10 17:45:42 -0400

    Adds alerts if user clicks on nav components before logging in

--- EHR-app/src/app/header/header.ts
+++ EHR-app/src/app/header/header.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthenticationService } from '../authentication/authentication.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-header',
-  imports: [FormsModule, RouterModule],
+  imports: [FormsModule, RouterModule, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header implements OnInit {
   
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(public authenticationService: AuthenticationService, private router: Router) {}
 
   ngOnInit() {}
+
+  unauthorizedClick(event: Event): void {
+    if (!this.authenticationService.isUserLoggedIn()) {
+      alert('Must login first');
+      return;
+    }
+  }
   
 }
