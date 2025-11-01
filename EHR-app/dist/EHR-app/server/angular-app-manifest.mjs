a90cd6db34afc732ca76d85eed5a555a232179a4
chance-404 <chance404404@gmail.com>
2025-11-01 15:53:07 -0400

    fixes issue with JWT token and userRole

--- EHR-app/dist/EHR-app/server/angular-app-manifest.mjs
+++ EHR-app/dist/EHR-app/server/angular-app-manifest.mjs
@@ -46,14 +46,14 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 24812, hash: '330e3c0eb207836363cc05a28fcddef5da56317ead0b8d569df31b44db5d975f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17222, hash: 'bc9a3934d1d2f75407ec38c4ad9eca9853afc3188ed8b4f32a0d41104b4ab849', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'logout/index.html': {size: 30891, hash: '9f4a8c407bb789371edc3be38df7cc1fdd36e41d5db3fb7077ce8510eade4cab', text: () => import('./assets-chunks/logout_index_html.mjs').then(m => m.default)},
-    'flow-board/index.html': {size: 30891, hash: '298358341a4b1ca24f118bfb7e422bdd74f154f15fe303aa726b819657a6352b', text: () => import('./assets-chunks/flow-board_index_html.mjs').then(m => m.default)},
-    'registration/index.html': {size: 30891, hash: '9f4a8c407bb789371edc3be38df7cc1fdd36e41d5db3fb7077ce8510eade4cab', text: () => import('./assets-chunks/registration_index_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 30891, hash: '7fe97ebf6e12823a1193f7fd40f5d930dd54765445813c039f884526677d9527', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
-    'dashboard/index.html': {size: 30891, hash: '9f4a8c407bb789371edc3be38df7cc1fdd36e41d5db3fb7077ce8510eade4cab', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
-    'orders/index.html': {size: 30891, hash: '9f4a8c407bb789371edc3be38df7cc1fdd36e41d5db3fb7077ce8510eade4cab', text: () => import('./assets-chunks/orders_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 24812, hash: 'd5de937ea9cc801167fe909e72ed98530606f274e106a441b3209beb9a033f18', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17222, hash: 'fc0f7cb8d72439ecf7a957959aebff2562cb5b0a67f5e6cfd0a5be4495bc6117', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'dashboard/index.html': {size: 31207, hash: '5ff07bb27f3c8e0af307192646830a159abc73b896d78abbdd065c4922f85290', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 31207, hash: '9f6441e777458eb4a701ac11dd8ec316f7a9014f7c99c96244df4f8e2b53dd68', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'registration/index.html': {size: 31207, hash: '5ff07bb27f3c8e0af307192646830a159abc73b896d78abbdd065c4922f85290', text: () => import('./assets-chunks/registration_index_html.mjs').then(m => m.default)},
+    'logout/index.html': {size: 31207, hash: '5ff07bb27f3c8e0af307192646830a159abc73b896d78abbdd065c4922f85290', text: () => import('./assets-chunks/logout_index_html.mjs').then(m => m.default)},
+    'orders/index.html': {size: 31207, hash: '5ff07bb27f3c8e0af307192646830a159abc73b896d78abbdd065c4922f85290', text: () => import('./assets-chunks/orders_index_html.mjs').then(m => m.default)},
+    'flow-board/index.html': {size: 31207, hash: '7abef1dc95e2edf251530ca76ce5a9a5443a46c3b8dc04102ec63487806d7599', text: () => import('./assets-chunks/flow-board_index_html.mjs').then(m => m.default)},
     'styles-ZK3N7HDU.css': {size: 8372, hash: '8/LtCGNtWyg', text: () => import('./assets-chunks/styles-ZK3N7HDU_css.mjs').then(m => m.default)}
   },
 };
--- EHR-app/src/environments/environment.production.ts
+++ EHR-app/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiServerUrl: 'https://ehr-backend-v0-0-1.onrender.com'
+  apiServerUrl: 'https://ehr-backend.chance404.cc'
 };
